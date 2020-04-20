3dce910bfc202077e1615020b16b31d74ac73ca8
serbulas1 <serbulas1@gmail.com>
2020-04-20 12:23:05 +0200

    Fixed spacing

--- 1-entity-framework-enterprise-m1-exercise-files/M1After/MVC Simple Repo/MvcSalesApp/Controllers/CustomersWithOrdersController.cs
+++ 1-entity-framework-enterprise-m1-exercise-files/M1After/MVC Simple Repo/MvcSalesApp/Controllers/CustomersWithOrdersController.cs
@@ -4,25 +4,29 @@ using MvcSalesApp.Data;
 
 namespace MvcSalesApp.Controllers
 {
-  public class CustomersWithOrdersController : Controller
-  {
-    private CustomerWithOrdersData repo = new CustomerWithOrdersData();
+    public class CustomersWithOrdersController : Controller
+    {
+        private CustomerWithOrdersData repo = new CustomerWithOrdersData();
 
-    // GET: CustomersWithOrders
-    public ActionResult Index() {
-      return View(repo.GetAllCustomers());
-    }
+        // GET: CustomersWithOrders
+        public ActionResult Index()
+        {
+            return View(repo.GetAllCustomers());
+        }
 
-    // GET: CustomersWithOrders/Details/5
-    public ActionResult Details(int? id) {
-      if (id == null) {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-      }
-      var customer = repo.FindCustomer(id);
-      if (customer == null) {
-        return HttpNotFound();
-      }
-      return View(customer);
+        // GET: CustomersWithOrders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var customer = repo.FindCustomer(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
     }
-  }
 }
